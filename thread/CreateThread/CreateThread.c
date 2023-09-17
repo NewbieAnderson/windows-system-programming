f2f67f1c14443087178d88e85da820aa26f05c3d
NewbieAnderson <engineeringsong@gmail.com>
2023-09-17 22:05:22 +0900

    added thread creation example codes

---
+++ thread/CreateThread/CreateThread.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int g_data = 12;
+
+DWORD WINAPI routine(LPVOID arg)
+{
+    int i;
+    printf("message : \"%s\"\n", (const char*)arg);
+    for (i = 0; i < 10; ++i)
+        ++g_data;
+    printf("g_data in new thread %d\n", g_data);
+    return 0;
+}
+
+int main(void)
+{
+    DWORD tid = 0;
+    HANDLE threadHandle = NULL;
+    const char* msg = "CreateThread()!";
+    /* _beginthreadex() is more recommended! */
+    threadHandle = CreateThread(NULL, 0, routine, (LPVOID)msg, 0, &tid);
+    if (threadHandle == NULL) {
+        printf("failed to create new thread!\n");
+        return 1;
+    }
+    WaitForSingleObject(threadHandle, INFINITE);
+    CloseHandle(threadHandle);
+    threadHandle = NULL;
+    printf("g_data in main thread : %d\n", g_data);
+    return 0;
+}
