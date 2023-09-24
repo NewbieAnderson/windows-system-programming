a34c716d1d41b20c6e8bd469394ed9eaf0cb6dbc
NewbieAnderson <engineeringsong@gmail.com>
2023-09-24 19:58:50 +0900

    changed _beginthreadex() code

--- thread/_beginthreadex/_beginthreadex.c
+++ thread/_beginthreadex/_beginthreadex.c
@@ -1,15 +1,9 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
 
-int g_data = 12;
-
 DWORD WINAPI routine(LPVOID arg)
 {
-    int i;
     printf("message : \"%s\"\n", (const char*)arg);
-    for (i = 0; i < 10; ++i)
-        ++g_data;
-    printf("g_data in new thread %d\n", g_data);
     return 0;
 }
 
@@ -18,10 +12,7 @@ int main(void)
     DWORD tid = 0;
     HANDLE threadHandle = NULL;
     const char* msg = "_beginthreadex()!";
-    /*
-        more recommanded than CreateThread()
-        TLS(Therad Local Storage) is applied and thread-safe.
-    */
+    /* more recommanded than CreateThread() */
     threadHandle = _beginthreadex(NULL, 0, routine, (LPVOID)msg, 0, &tid);
     if (threadHandle == NULL) {
         printf("failed to create new thread!\n");
@@ -30,6 +21,5 @@ int main(void)
     WaitForSingleObject(threadHandle, INFINITE);
     CloseHandle(threadHandle);
     threadHandle = NULL;
-    printf("g_data in main thread : %d\n", g_data);
     return 0;
 }
