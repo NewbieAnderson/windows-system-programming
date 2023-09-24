b32fa3b4ef180a6b6fa3718f80202d4fdd577cc0
NewbieAnderson <engineeringsong@gmail.com>
2023-09-24 19:59:44 +0900

    added ThreadOnOff code

---
+++ thread/ThreadOnOff/ThreadOnOff.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+DWORD WINAPI routine(LPVOID arg)
+{
+    printf("Awaken!\n");
+    return 0;
+}
+
+int main(void)
+{
+    DWORD tid = 0;
+    HANDLE threadHandle = NULL;
+    threadHandle = _beginthreadex(NULL, 0, routine, NULL, 0, &tid);
+    if (threadHandle == NULL) {
+        printf("failed to create new thread!\n");
+        return 1;
+    }
+    SuspendThread(threadHandle);
+    printf("Sleeping for 5 seconds...\n");
+    Sleep(5000);
+    ResumeThread(threadHandle);
+    WaitForSingleObject(threadHandle, INFINITE);
+    CloseHandle(threadHandle);
+    threadHandle = NULL;
+    return 0;
+}
